6246fb11efe67f5b8ad5e87f42cea30b7dc8ce22
Pierre Poulain <p.poulain.contact@gmail.com>
2024-11-17 16:13:15 +0100

    feat(domain-storytelling): generate library

---
+++ libs/feature-domain-storytelling/src/lib/domain-storytelling.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import DomainStorytelling from './domain-storytelling';
+
+describe('DomainStorytelling', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<DomainStorytelling />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/feature-domain-storytelling/src/lib/domain-storytelling.tsx
@@ -0,0 +1,3 @@
+export default function DomainStorytelling() {
+  return <h1>Welcome to DomainStorytelling!</h1>;
+}
