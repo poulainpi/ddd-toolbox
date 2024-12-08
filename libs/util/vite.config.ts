79b9f02f9a22c974b48dab2b189778f1b2b0c7f9
Pierre Poulain <p.poulain.contact@gmail.com>
2024-12-08 14:21:03 +0100

    test: pass with no tests on ui and utils libs

--- libs/util/vite.config.ts
+++ libs/util/vite.config.ts
@@ -18,5 +18,6 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: { reportsDirectory: '../../coverage/libs/util', provider: 'v8' },
+    passWithNoTests: true,
   },
 })
