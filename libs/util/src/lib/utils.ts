be399533831545e685ddd15d949b2611d8d63d23
Pierre Poulain <p.poulain.contact@gmail.com>
2026-04-04 16:39:18 +0200

    feat: track canvas export events with Umami analytics

--- libs/util/src/lib/utils.ts
+++ libs/util/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function track(eventName: string, data?: Record<string, unknown>) {
+  window.umami?.track(eventName, data)
+}
